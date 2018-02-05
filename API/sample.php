14f02d55982c2df0bc4e0c5c333e568ff2d961d7
Josh <kbrmedia1@gmail.com>
2018-02-05 15:50:09 -0500

    Create sample.php

---
+++ API/sample.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Include the main class
+ */
+include("Shortener.php");
+/**
+ * Instantiate it
+ * @var kbrmedia
+ */
+$shortener = new kbrmedia\Shortener();
+/**
+ * Set the URL & API key
+ */
+$shortener->setURL("http://url.kbr/api");
+$shortener->setKey("Fgvsld81Hvex");
+/**
+ * Simple call
+ */
+echo $shortener->shorten("https://gempixel.com");
+
+/**
+ * Advanced call
+ */
+// Custom Alias
+$shortener->setCustom("gempixel");
+
+// Format: text or json
+$shortener->setFormat("text");
+
+echo $shortener->shorten("https://gempixel.com");
+
+
+/**
+ * Unshorten call
+ */
+
+echo $shortener->unshorten("http://url.kbr/gempixel");
